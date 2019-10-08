75569bf2ad6928386579454abb6ecc3a5804ad67
Your Name <you@example.com>
2019-10-08 10:55:05 +0200

    ajout conseils travail en équipe

--- mpequipe/resources/views/espace-admin.blade.php
+++ mpequipe/resources/views/espace-admin.blade.php
@@ -7,13 +7,49 @@
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <h1>ESPACE ADMIN</h1>
-        <nav>
-            <ul>
-                <li><a href="<?php echo url('/') ?>">accueil</a></li>
-            </ul>
-        </nav>
-    </header>
+    <!-- TOUTE MA PAGE SERA GEREE AVEC VUEJS -->
+    <div id="app">
+        <header>
+            <h1>ESPACE ADMIN</h1>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo url('/') ?>">accueil</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+<?php
+// DANS LARAVEL, BLADE EST AUSSI UN MOTEUR DE TEMPLATE COTE SERVEUR
+// ET BLADE UTILISE LES {{ }}
+// POUR PRESERVER LES {{ }} POUR VUEJS
+// => ON VA EN FAIT UTILISER @{{ }}
+// => BLADE VA ENLEVER @
+// => ET GARDER LES {{ }} POUR VUEJS
+$coucou = "le texte à la place de coucou";
+?> 
+<h3><?php echo $coucou ?></h3>   
+<h3>{{ $coucou }}</h3>
+<h3>@{{ coucou }}</h3>
+
+        </main>  
+
+    </div>
+
+    <!-- on charge le code de vuejs -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <!-- et ensuite je peux ajouter mon code js -->
+    <script>
+// ON GARDE LE CODE DANS LE HTML
+// ET ENSUITE ON LE DEPLACERA DANS UN FICHIER A PART
+var app = new Vue({
+  el: '#app',       // vuejs va gérer toute la balise div#app
+  data: {
+    // ici on va rajouter nos variables gérées par vuejs
+    coucou: 'le texte donné avec VueJS',  
+    message: 'Hello Vue!'
+  }
+})
+
+    </script>
 </body>
 </html>
