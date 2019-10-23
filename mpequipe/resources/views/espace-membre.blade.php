f970113db40a692076ffe07e101998ddba7ba4bc
Your Name <you@example.com>
2019-10-23 10:27:14 +0200

    ajout de VueJS dans espace-membre

--- mpequipe/resources/views/espace-membre.blade.php
+++ mpequipe/resources/views/espace-membre.blade.php
@@ -7,14 +7,42 @@
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <h1>ESPACE MEMBRE</h1>
-        <nav>
-            <ul>
-                <li><a href="<?php echo url('/') ?>">accueil</a></li>
-                <li><a href="<?php echo url('/deconnexion') ?>">deconnexion</a></li>
-            </ul>
-        </nav>
-    </header>
+    <div id="app">
+        <header>
+            <h1>ESPACE MEMBRE</h1>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo url('/') ?>">accueil</a></li>
+                    <li><a href="<?php echo url('/deconnexion') ?>">deconnexion</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section>
+                <h3>FORMULAIRE DE CREATION D'UNE ANNONCE</h3>
+                <form action="" method="POST">
+                    <button type="submit">PUBLIER UNE ANNONCE</button>
+                </form>
+            </section>
+        </main>
+        <footer>
+        <!-- IL Y A UN CONFLOT ENTRE BLADE ET VUEJS -->
+        <!-- IL FAUT AJOUTER @ POUR QUE BLADE NE S'ACTIVE PAS SUR LES DELIMITEURS POUR VUEJS -->
+            @{{ message }}
+        </footer>
+    </div><!-- fin du container #app -->
+
+    <!-- CHARGER LE CODE DE VUEJS -->
+    <!-- https://fr.vuejs.org/v2/guide/index.html -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script>
+// ON PEUT ENSUITE COMMENCER A UTILISER VUEJS
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue !'
+  }
+});
+    </script>
 </body>
 </html>
