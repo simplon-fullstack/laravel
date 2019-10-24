905d268fa1377b4869d162a93eac24623671d5e8
Your Name <you@example.com>
2019-10-24 14:45:50 +0200

    ajout css

--- mpequipe/resources/views/accueil.blade.php
+++ mpequipe/resources/views/accueil.blade.php
@@ -15,6 +15,7 @@
             <ul>
                 <li><a href="<?php echo url('/') ?>">accueil</a></li>
                 <li><a href="<?php echo url('/annonces') ?>">annonces</a></li>
+                <li><a href="<?php echo url('/register') ?>">inscription</a></li>
                 <li><a href="<?php echo url('/espace-membre') ?>">espace membre</a></li>
                 <li><a href="<?php echo url('/galerie') ?>">galerie</a></li>
                 <li><a href="<?php echo url('/contact') ?>">contact</a></li>
